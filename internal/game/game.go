c147cfcb2b76c6edbfaaf308f102bc260afa54ca
Jaan Lavaerts <jaan.lavaerts2002@gmail.com>
2025-05-30 00:06:29 +0200

    more game logic

--- internal/game/game.go
+++ internal/game/game.go
@@ -6,6 +6,30 @@ import (
 	"github.com/JaanLavaerts/ticktickbrick/internal/data"
 )
 
+type RoomState int
+
+const (
+	WAITING    RoomState = iota // auto increment = 0
+	INPROGRESS                  // = 1
+	ENDED                       // = 2
+)
+
+type Room struct {
+	Users            []User
+	CurrentTurn      int // index of a user in []Users
+	MentionedPlayers []data.Player
+	CurrentTeam      data.Team
+	State            RoomState
+	StartTime        time.Time
+}
+
+type User struct {
+	Id          string
+	Username    string
+	Lives       int
+	HasAnswered bool // has the player answered yet this round
+}
+
 func StartGame(users []User, team data.Team) Room {
 	room := &Room{
 		Users:            users,
@@ -31,4 +55,53 @@ func NextTurn(room *Room, teams []data.Team) {
 	} else {
 		room.State = RoomState(ENDED)
 	}
+}
+
+func SubmitAnswer(room *Room, userId string, player data.Player) (bool, error) {
+	answer := data.PlayerPlayedFor(player, room.CurrentTeam)
+	room.MentionedPlayers = append(room.MentionedPlayers, player)
+
+	//TODO: remove life of user when answer is wrong
+	//TODO: set hasAnswered to true
+
+	return answer, nil
+}
+
+func RemoveLife(room *Room, userId string) {
+	user := getUserById(room.Users, userId)
+	user.Lives--
+}
+
+func IsGameOver(room *Room) bool {
+	var aliveCount int
+
+	for i := range room.Users {
+		user := room.Users[i]
+		if user.Lives != 0 {
+			aliveCount++
+		}
+	}
+	return aliveCount <= 1
+}
+
+func GetWinner(room *Room) string {
+	var winnerUsername string
+
+	for i := range room.Users {
+		user := room.Users[i]
+		if user.Lives != 0 {
+			winnerUsername = user.Username
+		}
+	}
+	return winnerUsername
+}
+
+func getUserById(users []User, userId string) User {
+	var user User
+	for i := range users {
+		if users[i].Id == userId {
+			user = users[i]
+		}
+	}
+	return user
 }
--- internal/game/user.go
+++
@@ -1,8 +0,0 @@
-package game
-
-type User struct {
-	Id          string
-	Username    string
-	Lives       int
-	HasAnswered bool // has the player answered yet this round
-}
