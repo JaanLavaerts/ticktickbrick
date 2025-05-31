3858151274d5fa3a2b3b730e9e0ff93e873c18f9
Jaan Lavaerts <jaan.lavaerts2002@gmail.com>
2025-05-31 14:35:53 +0200

    newTestRoom() func

--- internal/game/game_test.go
+++ internal/game/game_test.go
@@ -6,35 +6,36 @@ import (
 	"github.com/JaanLavaerts/ticktickbrick/internal/data"
 )
 
-var teams, _ = data.LoadData[data.Team]("../../assets/teams.json")
-var team = data.RandomTeam(teams)
+func newTestRoom() Room {
+	users := []User{
+		{
+			Id:       "1",
+			Username: "userOne",
+			Lives:    3,
+		},
+		{
+			Id:       "2",
+			Username: "userTwo",
+			Lives:    3,
+		},
+		{
+			Id:       "3",
+			Username: "userThree",
+			Lives:    3,
+		},
+	}
 
-var userOne = User{
-	Id:          "1",
-	Username:    "userOne",
-	Lives:       3,
-	HasAnswered: false,
-}
-var userTwo = User{
-	Id:          "2",
-	Username:    "userTwo",
-	Lives:       3,
-	HasAnswered: false,
-}
-var userThree = User{
-	Id:          "3",
-	Username:    "userThree",
-	Lives:       3,
-	HasAnswered: false,
-}
+	teams, _ := data.LoadData[data.Team]("../../assets/teams.json")
+	team := data.RandomTeam(teams)
 
-var users = []User{userOne, userTwo, userThree}
-var room = StartGame(users, team)
+	return StartGame(users, team)
+}
 
 func TestNextTurnUserAlive(t *testing.T) {
+	room := newTestRoom()
 	// check if nextturn is on index 1
 	nextTurn := room.CurrentTurn + 1
-	NextTurn(&room, team)
+	NextTurn(&room, room.CurrentTeam)
 
 	if room.CurrentTurn != nextTurn {
 		t.Errorf("got %v, wanted %v", room.CurrentTurn, nextTurn)
@@ -42,11 +43,12 @@ func TestNextTurnUserAlive(t *testing.T) {
 }
 
 func TestNextTurnUserDead(t *testing.T) {
+	room := newTestRoom()
 	// check if nextturn is on index 2 if index 1 is dead
 	nextTurn := room.CurrentTurn + 2
 
 	room.Users[1].Lives = 0
-	NextTurn(&room, team)
+	NextTurn(&room, room.CurrentTeam)
 
 	if room.CurrentTurn != nextTurn {
 		t.Errorf("got %v, wanted %v", room.CurrentTurn, nextTurn)
@@ -54,23 +56,25 @@ func TestNextTurnUserDead(t *testing.T) {
 }
 
 func TestNextTurnUserAliveWrap(t *testing.T) {
+	room := newTestRoom()
 	// start at last player, check if it wraps to first player
 	room.CurrentTurn = len(room.Users) - 1
 	nextTurn := 0
 
-	NextTurn(&room, team)
+	NextTurn(&room, room.CurrentTeam)
 
 	if room.CurrentTurn != nextTurn {
 		t.Errorf("got %v, wanted %v", room.CurrentTurn, nextTurn)
 	}
 }
 func TestNextTurnUserDeadWrap(t *testing.T) {
+	room := newTestRoom()
 	// start at last player, check if it wraps to second player if first player is dead
 	room.CurrentTurn = len(room.Users) - 1
 	nextTurn := 1
 
 	room.Users[0].Lives = 0
-	NextTurn(&room, team)
+	NextTurn(&room, room.CurrentTeam)
 
 	if room.CurrentTurn != nextTurn {
 		t.Errorf("got %v, wanted %v", room.CurrentTurn, nextTurn)
